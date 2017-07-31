f1daab823162b7a5b968fce163ca30332e3f4283
Robbie Jaeger <jaeger.rob@gmail.com>
2017-07-31 09:17:57 -0600

    Refactor js and add clear buttons

--- public/js/search.js
+++ public/js/search.js
@@ -4,6 +4,11 @@ $(function(config){
 
   hideSearch();
 
+  $('.clear-search img, .search-results-container img').on('click', function() {
+    $('.sidebar-navigation--search input').val('');
+    hideSearch();
+  });
+
   $('.sidebar-navigation--search input').on('input', function() {
     var query = $(this).val();
     onQueryChange(query);
@@ -11,40 +16,51 @@ $(function(config){
 
   function onQueryChange(query) {
     if (query.length) {
-      index.search(query, {"hitsPerPage": "10", "page": "0", "typoTolerance": "false"}, onResult);
+      index.search(query, {'hitsPerPage': '10', 'page': '0', 'typoTolerance': 'false'}, onResult);
     } else {
       hideSearch();
     }
   };
 
   function onResult(err, data) {
-    if (err) {
-      console.error(err);
-    } else {
-      displayResults(data);
-    }
+    if (err) { return console.error(err) }
+    displayResults(data);
   };
 
   function displayResults(data) {
     showSearch();
     $('.search-results').html('');
     if (data.hits.length) {
-      data.hits.forEach(function(result) {
-        $('.search-results').append(`<li>
-                                       <a href="${result.url}">${result.title}</a>
-                                       ${result.raw_html}
-                                     </li>`);
-      });
+      renderHits(data.hits);
     } else {
-      $('.search-results-container').append('<p>No search results for that query</p>');
+      renderNoResultsFound();
     }
   };
 
+  function renderNoResultsFound() {
+    $('.search-results').append('<p>No search results for that query</p>');
+  }
+
+  function renderHits(hits) {
+    hits.forEach(function(hit) {
+      $('.search-results').append(hitTemplate(hit))
+    });
+  }
+
+  function hitTemplate(hit) {
+    return `<li>
+      <a href="${hit.url}">${hit.title}</a>
+      ${hit.raw_html}
+    </li>`
+  }
+
   function showSearch() {
     $('.search-results-container').show();
+    $('.clear-search img').show();
   };
 
   function hideSearch() {
     $('.search-results-container').hide();
+    $('.clear-search img').hide();
   };
 }(window.ALGOLIA_CONFIG));
