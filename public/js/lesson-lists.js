83d03cbbedf49599ead58c3c3003c8b52a33b99d
Robbie Jaeger <jaeger.rob@gmail.com>
2018-01-13 21:55:24 -0700

    Lessons are hidden and shown based on preference

--- public/js/lesson-lists.js
+++ public/js/lesson-lists.js
@@ -1,9 +1,33 @@
-$(function(){
+$(function() {
   if (localStorage.getItem('fe-curriculum-lessons') === null) {
     localStorage.setItem('fe-curriculum-lessons', JSON.stringify({showModule: 0}));
   }
 
-  $('.lesson-selector').on('click', function(){
-    
+  showOrHideLessonContainer(getLessonPreference());
+
+  $('.lesson-selector').on('click', function(e) {
+    let moduleSelection = $(e.target).data('module');
+    setLessonPreference(moduleSelection);
+    showOrHideLessonContainer(moduleSelection);
   });
+
+  function showOrHideLessonContainer(moduleSelection) {
+    if (moduleSelection === 0) { return $('.lesson-container').show() };
+
+    $('.lesson-container').each(function(idx, el) {
+      if ($(el).data('module') === moduleSelection) {
+        $(el).show();
+      } else {
+        $(el).hide();
+      }
+    });
+  };
+
+  function setLessonPreference(moduleSelection) {
+    localStorage.setItem('fe-curriculum-lessons', JSON.stringify({showModule: moduleSelection}));
+  };
+
+  function getLessonPreference() {
+    return JSON.parse(localStorage.getItem('fe-curriculum-lessons')).showModule;
+  };
 }());
